a7418b8130ce44d761887d7ce102195862446fa7
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-26 19:03:39 -0300

    ex2

--- main.sql
+++ main.sql
@@ -4,3 +4,12 @@ SELECT users.id, users.name, cities.name AS city
 FROM users
 JOIN cities
 ON users."cityId" = cities.id;
+
+-- EXERCÍCIO 2
+
+SELECT testimonials.id, writer.name AS writer, recipient.name AS recipient, testimonials.message
+FROM testimonials
+JOIN users AS writer
+ON testimonials."writerId" = writer.id
+JOIN users AS recipient
+ON testimonials."recipientId" = recipient.id;
